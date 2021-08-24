28bbdd570f2ba4d2ff394e1071f22809338089be
chrisvanetten <christophervanetten@gmail.com>
2021-08-24 13:17:43 -0500

    added framework for employee classes, package.json

---
+++ index.js
@@ -0,0 +1,4 @@
+const Employee = require('./lib/employeeClass')
+const Manager = require('./lib/managerClass')
+const Engineer = require('./lib/engineerClass')
+const Intern = require('./lib/internClass')
